51194b37e55e283a74ab8f11a3a16e447cbd1d33
mirzomumin <mirzomumin@list.ru>
2022-12-04 11:37:22 +0500

    Update DB settings

--- config/settings.py
+++ config/settings.py
@@ -103,9 +103,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'candidate_testing',
-        'USER': 'postgres',
-        'PASSWORD': '1sakura1',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': 'localhost',
         'PORT': '5432',
     }
